5d0f7afe80a79c6cceb06b102cf6103c04f16326
Mathematicienne <55539697+Mathematicienne@users.noreply.github.com>
2019-10-03 01:06:55 +0300

    Занятие 8. Функции и рекурсия Задача «Длина отрезка»

--- _8.1.py
+++ _8.1.py
@@ -4,10 +4,5 @@ def distance(x1, y1, x2, y2):
     y = y1 - y2
     c = sqrt(fabs(x) * fabs(x) + fabs(y) * fabs(y))
     return c
-x1 = float(input())
-y1 = float(input())
-x2 = float(input())
-y2 = float(input())
-print(distance(x1, y1, x2, y2))
-
+print(distance(float(input()), float(input()), float(input()), float(input())))
 
