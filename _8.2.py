ec93c6b3763aefc6687a5d73fd3fd5a1e584dd32
Mathematicienne <kristina.bogacheva.2014@mail.ru>
2019-10-03 00:44:33 +0300

    Занятие 8. Функции и рекурсия

---
+++ _8.2.py
@@ -0,0 +1,13 @@
+def power(a, n):
+    a1 = 1
+    if n > 0:
+        for i in range(n):
+            a1 *= a
+    elif n < 0:
+        a = 1/a
+        for i in range(-n):
+            a1 *= a
+    return(a1)
+a = float(input())
+n = int(input())
+print(power(a, n))
